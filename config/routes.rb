b3718e7e24a10660e2880be33b3df3095bc294b8
Nestor Walker <naw4t@virginia.edu>
2018-05-22 11:02:58 -0400

    V2 user skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,13 @@ Rails.application.routes.draw do
 
     resources :items, only: :show
   end
+  namespace :v2 do
+    resources :libraries
+    resources :users
+
+
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
