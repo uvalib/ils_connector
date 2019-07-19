31a9d353d27dc35777d013a795653a5657541773
Nestor Walker <naw4t@virginia.edu>
2019-07-19 14:17:18 -0400

    override credentials with environment variables

--- config/application.rb
+++ config/application.rb
@@ -48,5 +48,5 @@ end
 
 # Provides the credential value for a given key scoped to the current rails environment
 def env_credential key
-    Rails.application.credentials[Rails.env.to_sym][key.to_sym]
+  ENV[key.upcase] || Rails.application.credentials[Rails.env.to_sym][key.to_sym]
 end
