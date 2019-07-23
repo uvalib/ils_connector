827aec93224433809ddef4c7ea28531e2d693f6e
Nestor Walker <naw4t@virginia.edu>
2019-07-23 12:14:31 -0400

    make libraries thread safe

--- app/models/v2/library.rb
+++ app/models/v2/library.rb
@@ -1,7 +1,22 @@
 class V2::Library < ActiveYaml::Base
-  include ActiveModel::Serializers::Xml
-
 
   set_root_path "app/data/"
   set_filename "libraries"
+
+  class <<self
+    alias_method :all_yaml, :all
+    alias_method :find_by_yaml, :find_by
+  end
+
+  # store libraries in memory. ActiveYaml has thread issues
+  def self.all
+    @@libraries ||= all_yaml
+  end
+  def self.find_by opt
+    all
+    key = opt.first.first
+    value = opt.first.last
+    library = @@libraries.find {|l| l.send(key) == value}
+    library
+  end
 end
