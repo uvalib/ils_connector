e0044ae819d3e106ce6d10ffc0e5a112ae4f6b13
Nestor Walker <naw4t@virginia.edu>
2019-07-24 14:10:06 -0400

    remove unnecessary request id form logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch('LOG_LEVEL') { :info }
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  #config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
