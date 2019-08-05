c5d5d24583b4f719650c45008ec8e76dab208f34
Nestor Walker <naw4t@virginia.edu>
2019-08-05 10:59:52 -0400

    items only, 404 everything else

--- app/controllers/v2_application_controller.rb
+++ app/controllers/v2_application_controller.rb
@@ -3,4 +3,13 @@ class V2ApplicationController < ApplicationController
   include ActionController::ImplicitRender
   include XmlHelpers
 
+  before_action :items_only
+
+  def items_only
+    if controller_name != 'items'
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
+
 end
