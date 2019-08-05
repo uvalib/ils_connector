c15e74df60eb28e73222cf1046d8cd64d7f77dc3
Nestor Walker <naw4t@virginia.edu>
2019-08-05 14:18:22 -0400

    Revert "items only, 404 everything else"
    This reverts commit c5d5d24583b4f719650c45008ec8e76dab208f34.

--- app/controllers/v2_application_controller.rb
+++ app/controllers/v2_application_controller.rb
@@ -3,13 +3,4 @@ class V2ApplicationController < ApplicationController
   include ActionController::ImplicitRender
   include XmlHelpers
 
-  before_action :items_only
-
-  def items_only
-    if controller_name != 'items'
-      raise ActionController::RoutingError.new('Not Found')
-    end
-  end
-
-
 end
