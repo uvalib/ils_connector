b63b84b7b9778fbbfbf43a683fddf23d47de09d2
Nestor Walker <naw4t@virginia.edu>
2019-09-26 15:24:47 -0400

    availability response and basic formatting tests

---
+++ spec/requests/v4/availability_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "V4::Availability", type: :request do
+  describe "GET /v4/availability/:id" do
+    let(:title_id) {'4684522'}
+    before do
+      get v4_availability_path(id: title_id, format: :json), headers: {'ACCEPT': 'application/json'}
+    end
+
+    it 'works' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'contains the correct keys' do
+
+      v4_response = JSON.parse(response.body)['availability']
+
+      expect(v4_response.keys).to match_array(%w(title_id columns holdings))
+
+      v4_response['holdings'].each do |holding|
+        holding['fields'].each do |item|
+          expect(item.keys).to match_array(%w(name value visible type))
+        end
+      end
+    end
+  end
+end
