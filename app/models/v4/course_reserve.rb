5c2faf069f9b6bb1335af8682d0835498c3ec889
Lou Foster <lf6f@virginia.edu>
2019-10-16 15:14:45 -0400

    dont send multiple versins of a book in reserves. instead increment a copies counter

--- app/models/v4/course_reserve.rb
+++ app/models/v4/course_reserve.rb
@@ -36,11 +36,8 @@ class V4::CourseReserve < SirsiBase
             end
 
             # extract raw reserve data into course, instructor and reserved item
-            course = {id: fields['course']['fields']['courseID'], name: fields['course']['fields']['name']}  
-            puts "COURSE #{course}"
-            
+            course = {id: fields['course']['fields']['courseID'], name: fields['course']['fields']['name']}              
             instructor = {name: fields['instructor']['fields']['name']}   
-            puts "INSTRUCTOR #{instructor}"
 
             item = {title: fields['title'], author: fields['author']}
             item_data = reserve_info['item']
@@ -48,70 +45,51 @@ class V4::CourseReserve < SirsiBase
             item[:callNumber] =  item_data['fields']['call']['fields']['callNumber']
             item[:reserveDesk] = fields['reserveCollection']['key']
             item[:circulationRule] = fields['circulationRule']['key']
-            puts "RESERVE #{item}"
-
+            item[:copies] = 1
 
             # build a heirarchy based on type
+            tgt_reserves = nil
             if type == "INSTRUCTOR_NAME"
                # instructor centric: instructor->course->reserves
-               tgt_inst = nil
-               out.each do |ins|
-                  if ins[:name] == instructor[:name]
-                     tgt_inst = ins  
-                     break 
-                  end 
-               end 
-               if tgt_inst.nil? 
+               tgt_inst = out.find{|ins| ins[:name] == instructor[:name]}
+               if tgt_inst.blank? 
                   instructor[:courses] = []
                   out << instructor
                   tgt_inst = instructor 
                end
 
-               tgt_course = nil 
-               tgt_inst[:courses].each do |c|
-                  if c[:id] == course[:id]
-                     tgt_course = c  
-                     break 
-                  end 
-               end 
-               if tgt_course.nil? 
+               tgt_course = tgt_inst[:courses].find{|c| c[:id] == course[:id]} 
+               if tgt_course.blank? 
                   course[:reserves] = []
                   tgt_course = course 
                   tgt_inst[:courses] << tgt_course
                end
-               tgt_course[:reserves] << item
+               tgt_reserves = tgt_course[:reserves]
             else
                # Course centric: course->instructor->reserves
-               tgt_course = nil
-               out.each do |c|
-                  if c[:id] == course[:id]
-                     tgt_course = c  
-                     break 
-                  end 
-               end 
-               if tgt_course.nil? 
+               tgt_course = out.find { |c| c[:id] == course[:id] }
+               if tgt_course.blank? 
                   course[:instructors] = []
                   out << course
                   tgt_course = course 
                end
 
-               tgt_inst = nil 
-               tgt_course[:instructors].each do |ins|
-                  if ins[:name] == instructor[:name]
-                     tgt_inst = ins  
-                     break 
-                  end 
-               end 
-               if tgt_inst.nil? 
+               tgt_inst = tgt_course[:instructors].find{|ins| ins[:name] == instructor[:name]}
+               if tgt_inst.blank? 
                   instructor[:reserves] = []
                   tgt_inst = instructor 
                   tgt_course[:instructors] << tgt_inst
                end
-               tgt_inst[:reserves] << item
-            end      
+               tgt_reserves = tgt_inst[:reserves]
+            end 
+            existing = tgt_reserves.find { |r| r[:catalogKey] == item[:catalogKey] }
+            if existing.blank? 
+               tgt_reserves << item     
+            else 
+               existing[:copies] += 1
+            end
          end
       end
-      puts "OUT #{out}"
       return out
    end
 end
