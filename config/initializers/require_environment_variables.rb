bdc2c8b840829ed0d57b545512c355066a3fbf33
Nestor Walker <naw4t@virginia.edu>
2020-06-22 15:32:48 -0400

    fix env variable check timing

--- config/initializers/require_environment_variables.rb
+++ config/initializers/require_environment_variables.rb
@@ -1,27 +1,25 @@
-Rails.application.config.before_configuration do
-  if Rails.env.production?
-    required_env = %w(
-    SIRSI_WEB_SERVICES_BASE
-    SIRSI_USER
-    SIRSI_PASSWORD
-    SIRSI_CLIENT_ID
-    SIRSI_LIBRARY
-    SIRSI_SCRIPT_URL
-    USERINFO_URL
-    SERVICE_API_KEY
-    FIREHOSE_BASE_URL
-    PDA_BASE_URL
-    V4_JWT_KEY
-    )
-    missing_list = []
-    required_env.each do |required|
-      if !ENV[required].present?
-        missing_list << required
-      end
+if Rails.env.production?
+  required_env = %w(
+  SIRSI_WEB_SERVICES_BASE
+  SIRSI_USER
+  SIRSI_PASSWORD
+  SIRSI_CLIENT_ID
+  SIRSI_LIBRARY
+  SIRSI_SCRIPT_URL
+  USERINFO_URL
+  SERVICE_API_KEY
+  FIREHOSE_BASE_URL
+  PDA_BASE_URL
+  V4_JWT_KEY
+  )
+  missing_list = []
+  required_env.each do |required|
+    if !ENV[required].present?
+      missing_list << required
     end
+  end
 
-    if missing_list.any?
-      raise "Required environment variable(s) \"#{missing_list.to_sentence}\" not set."
-    end
+  if missing_list.any?
+    raise "Required environment variable(s) \"#{missing_list.to_sentence}\" not set."
   end
 end
