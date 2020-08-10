8c8dd799b2d6aacb8d5a2a7c9881b3d19f96c216
Nestor Walker <naw4t@virginia.edu>
2020-08-10 16:30:54 -0400

    Revert "VIRGONEW-1588 Remove Shadowed records"
    This reverts commit 693c1172ffbfe07a5edab67651455318c89c3e54.
    Revert shadowed record removal

--- app/models/v4/availability.rb
+++ app/models/v4/availability.rb
@@ -27,7 +27,7 @@ class V4::Availability < SirsiBase
                     includeAvailabilityInfo: 'true',
                     includeCallNumberSummary: 'true',
                     includeFields: '*',
-                    includeShadowed: 'NONE',
+                    includeShadowed: 'BOTH',
                     includeBoundTogether: 'true',
                     marcEntryID: '949,985,911'
   }
