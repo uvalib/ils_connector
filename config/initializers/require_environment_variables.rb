102c92d036819205ca8be4041d20fd245cd5ade3
Nestor Walker <naw4t@virginia.edu>
2020-08-11 10:47:13 -0400

    formatting

--- config/initializers/require_environment_variables.rb
+++ config/initializers/require_environment_variables.rb
@@ -1,29 +1,28 @@
 Rails.application.config.before_initialize do
-    required_env = %w(
-    SIRSI_WEB_SERVICES_BASE
-    SIRSI_SCRIPT_URL
-    SIRSI_USER
-    SIRSI_PASSWORD
-    SIRSI_CLIENT_ID
-    SIRSI_LIBRARY
-    AUTH_SHARED_SECRET
-    SECRET_KEY_BASE
-    USERINFO_URL
-    PDA_BASE_URL
-    V4_JWT_KEY
-    JWT_SECRET
-    )
-    # To be included later
-    missing_list = []
-    required_env.each do |required|
-      if !ENV[required].present?
-        missing_list << required
-      end
+  required_env = %w(
+  SIRSI_WEB_SERVICES_BASE
+  SIRSI_SCRIPT_URL
+  SIRSI_USER
+  SIRSI_PASSWORD
+  SIRSI_CLIENT_ID
+  SIRSI_LIBRARY
+  AUTH_SHARED_SECRET
+  SECRET_KEY_BASE
+  USERINFO_URL
+  PDA_BASE_URL
+  V4_JWT_KEY
+  JWT_SECRET
+  )
+  missing_list = []
+  required_env.each do |required|
+    if !ENV[required].present?
+      missing_list << required
     end
+  end
 
-    if missing_list.any?
-      msg = "Required environment variable(s) \"#{missing_list.to_sentence}\" not set."
-      Rails.logger.error(msg)
-      raise msg
-    end
+  if missing_list.any?
+    msg = "Required environment variable(s) \"#{missing_list.to_sentence}\" not set."
+    Rails.logger.error(msg)
+    raise msg
+  end
 end
